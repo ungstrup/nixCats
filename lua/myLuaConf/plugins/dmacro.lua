2b65f4b25c38fde5f5a0e51fe9c148e9ae7e2946
ungstrup <8708181+ungstrup@users.noreply.github.com>
2025-04-16 16:32:51 +0200

    trying to fix dmacro

--- lua/myLuaConf/plugins/dmacro.lua
+++ lua/myLuaConf/plugins/dmacro.lua
@@ -1,9 +1,8 @@
 return {
     {
         "dmacro.vim",
-        event = "DeferredUIEnter",
         config = function()
-        vim.keymap.set({ "i", "n" }, '<C-y>', '<Plug>(dmacro-play-macro)')
+            vim.keymap.set({ "i", "n" }, '<C-y>', '<Plug>(dmacro-play-macro)')
         end,
     },
 }
