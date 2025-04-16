7b1bc08536e3bb326987916d7a93bb6d4f9b2b64
ungstrup <8708181+ungstrup@users.noreply.github.com>
2025-04-16 17:01:47 +0200

    trying to fix dmacro

--- lua/myLuaConf/plugins/dmacro.lua
+++ lua/myLuaConf/plugins/dmacro.lua
@@ -1,7 +1,7 @@
 return {
     {
         "dmacro.vim",
-        keys = {{ '<c-y', '<Plug(dmacro-play-macro)', mode = { 'i', 'n'} }},
+        keys = {{ '<c-y', '<Plug>(dmacro-play-macro)', mode = { 'i', 'n'} }},
         -- config = function()
         --     vim.keymap.set({ "i", "n" }, '<C-y>', '<Plug>(dmacro-play-macro)')
         -- end,
