8287f1e96df6450e5e45005461b91c5a764b1864
ungstrup <8708181+ungstrup@users.noreply.github.com>
2025-05-08 15:51:12 +0200

    feat: add virtual lines for diagnostics

--- lua/myLuaConf/opts_and_keys.lua
+++ lua/myLuaConf/opts_and_keys.lua
@@ -124,6 +124,10 @@ vim.cmd([[command! Q q]])
 vim.keymap.set('n', 'k', "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
 vim.keymap.set('n', 'j', "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
 
+-- diagnostics
+vim.diagnostic.config({
+    virtual_lines = true,
+})
 -- Diagnostic keymaps
 vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, { desc = 'Go to previous diagnostic message' })
 vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = 'Go to next diagnostic message' })
